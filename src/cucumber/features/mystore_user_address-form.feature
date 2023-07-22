eca1f87a5691468a8146ff6acc996553fe5ce42f
Ka3san <tomasz.ka3@gmail.com>
2023-07-22 23:43:37 +0200

    workshop 1 main page opened in browser with url parametrized

--- src/cucumber/features/mystore_user_address-form.feature
+++ src/cucumber/features/mystore_user_address-form.feature
@@ -1,12 +1,12 @@
 Feature: Mystore user address form filling and check
 Scenario: Successful fill in address form of existing user and confirm operation
 Given https://mystore-testlab.coderslab.pl/index.php opened in Google Chrome browser
-When Sign In button clicked
-And Email and Password filled
-And SIGN IN button clicked
-And Addresses link from bottom 'YOUR ACCOUNT' block clicked
-And + Create new address button clicked
-And New address form filled with alias, address, city, zip/postal code, country, phone
-Then Check if the filled data is correct
-#And Delete the above address
-#And Check if the above address has been deleted
+#When Sign In button clicked
+#And Email and Password filled
+#And SIGN IN button clicked
+#And Addresses link from bottom 'YOUR ACCOUNT' block clicked
+#And + Create new address button clicked
+#And New address form filled with alias, address, city, zip/postal code, country, phone
+#Then Check if the filled data is correct
+##And Delete the above address
+##And Check if the above address has been deleted
--- src/test/java/pl/coderslab/testautomationworkshopsonlteaw39/workshop1/MyStoreUserAddressFormSteps.java
+++ src/test/java/pl/coderslab/testautomationworkshopsonlteaw39/workshop1/MyStoreUserAddressFormSteps.java
@@ -1,7 +1,46 @@
 package pl.coderslab.testautomationworkshopsonlteaw39.workshop1;
 
+import io.cucumber.java.en.Given;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class MyStoreUserAddressFormSteps {
+    private WebDriver driver;
+//    private MainPage mainPage;
+
+    @Given("^([^ ]+) opened in Google Chrome browser$")
+    public void openInBrowser(String url) {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+//        mainPage = new MainPage(driver);
+
+        driver.get(url);
+
+    }
 
 
+//    @When("Sign In button clicked")
+//
+//
+//    @And("Email and Password filled")
+//
+//
+//    @And("SIGN IN button clicked")
+//
+//
+//    @And("Addresses link from bottom 'YOUR ACCOUNT' block clicked")
+//
+//
+//    @And("+ Create new address button clicked")
+//
+//
+//    @And("New address form filled with alias, address, city, zip/postal code, country, phone")
+//
+//
+//    @Then("Check if the filled data is correct")
+//
 
 }
